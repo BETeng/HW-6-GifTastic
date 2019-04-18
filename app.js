c3bacb7218940ff98d3a3486749f61aa0ccac87b
BETeng <brian7.teng@gmail.com>
2019-04-18 01:20:41 -0500

    working search

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-var search = 'cat';//user input from form
-var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=hCyFEBdF8HQfMoLF7KsfUPThKdrfY5cW&q'+ search + '=&limit=10&offset=0&rating=G&lang=en';
+// var search = 'dog';//user input from form
+
 // api key = hCyFEBdF8HQfMoLF7KsfUPThKdrfY5cW
 var itemArray = [];
-
-$('#test').click(function(){
-
+$('#add-item').on('click', function(event){
+  event.preventDefault();
+  console.log('click');
+  var search = $('#response').val().trim();
+  var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=hCyFEBdF8HQfMoLF7KsfUPThKdrfY5cW&q='+search+'&limit=10&offset=0&rating=PG-13&lang=en'
+  
+  console.log("search "+search)
   $.ajax({
     url: queryURL,
     method: "GET"
   })
   
   .then(function(response){
-    console.log(queryURL)
+    console.log(queryURL);
+    console.log(response.data);
+    console.log('click2')
   })
+ 
 
 })
-
+//get item from textbox when submit button clicked and put into array
+//loop thru array and look 
 
 
